c88f212193d2823b306b500e1fe094e74c1b651b
rokf <snewix7@gmail.com>
2016-06-03 21:14:39 +0200

    battery fix on full

--- battery.lua
+++ battery.lua
@@ -32,12 +32,18 @@ batterywidgettimer:connect_signal("timeout",
     local read_text = fh:read("*l")
     local find_charging = string.match(read_text, "charged")
     local wanted = {string.match(read_text, "(%d+):(%d+)")} -- match hours and minutes only
-    local selected_icon = icons[ tonumber(wanted[1])+1 ] or icons[#icons]
-    local selected_color = colors[ tonumber(wanted[1])+1 ] or colors[#colors]
-    if find_charging ~= "charged" then
-      batterywidget:set_markup(string.format("<span color='%s'>%s</span> %sh %sm", selected_color, selected_icon, wanted[1], wanted[2]))
+
+    local selected_icon = icons[ (tonumber(wanted[1]) or 0)+1 ] or icons[#icons]
+    local selected_color = colors[ (tonumber(wanted[1]) or 0) +1 ] or colors[#colors]
+
+    if wanted[1] == nil and wanted[2] == nil then -- if its full
+      batterywidget:set_markup(string.format("<span color='%s'>%s</span> ", colors[#colors], charging_icon))
     else
-      batterywidget:set_markup(string.format("<span color='%s'>%s</span> %sh %sm", colors[#colors], charging_icon, wanted[1], wanted[2]))
+      if find_charging ~= "charged" then
+        batterywidget:set_markup(string.format("<span color='%s'>%s</span> %sh %sm", selected_color, selected_icon, wanted[1], wanted[2]))
+      else
+        batterywidget:set_markup(string.format("<span color='%s'>%s</span> %sh %sm", colors[#colors], charging_icon, wanted[1], wanted[2]))
+      end
     end
 
     fh:close()
